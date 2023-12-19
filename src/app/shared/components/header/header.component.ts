1b6c5e1d58a4a94237b7282a5ca34772f43b2d7e
Gonzalo Rando Serna <gonzalorando03@gmail.com>
2023-12-19 20:07:37 +0100

    asdfasdf

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { Usuario } from 'src/app/core/models/usuario.model';
 import { UserService } from 'src/app/core/services/user.service';
 
@@ -7,16 +8,19 @@ import { UserService } from 'src/app/core/services/user.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   login: boolean = false;
   user: Usuario | null = null
+  @Input() header1:boolean = true
 
-  constructor(private userService:UserService){
+  constructor(private userService:UserService, private location: Location){
     this.userService.getUser().subscribe(data => {
       this.user = data
       this.login = this.user !== null
     })
   }
+
+  ngOnInit(): void {}
 
   async logout(){
     await this.userService.desloguearUsuario()
