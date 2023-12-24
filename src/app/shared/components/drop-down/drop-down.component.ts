70ec441aff67fc78c3af7a416efd6eb1ed272836
Gonzalo Rando Serna <gonzalorando03@gmail.com>
2023-12-24 13:32:09 +0100

    Proyecto

---
+++ src/app/shared/components/drop-down/drop-down.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-drop-down',
+  templateUrl: './drop-down.component.html',
+  styleUrls: ['./drop-down.component.scss']
+})
+export class DropDownComponent {
+  @Input() titulo:string = ''
+  @Input() elements:DropDownElement[] = []
+}
+
+export interface DropDownElement {
+  titulo:string
+  hasLink:boolean
+  link?:string
+}
